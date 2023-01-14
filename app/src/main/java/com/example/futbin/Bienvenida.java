bafbc148048f181668a18abff71087a9709280fe
Daniel88871 <druizl01@elpuig.xeill.net>
2023-01-14 21:06:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/futbin/Bienvenida.java
@@ -0,0 +1,48 @@
+package com.example.futbin;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Bienvenida extends Fragment {
+
+    Button botonSiguiente;
+    NavController navController;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bienvenida, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        botonSiguiente = view.findViewById(R.id.BotonSiguiente);
+
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_bienvenida_to_seleccionDeEquipo);
+            }
+        });
+    }
+}
